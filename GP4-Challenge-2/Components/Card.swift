04f7d406957eadaf6b2edeaa9ce79e1911e1d144
amanda <amandacsr10@gmail.com>
2024-08-27 18:14:25 -0300

    Criação cards pesquisa

--- GP4-Challenge-2/Components/Card.swift
+++ GP4-Challenge-2/Components/Card.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct Card: View {
     let recipe: Recipe
     
+    
+    
     var body: some View {
         ZStack(alignment: .leading) {
                 RoundedRectangle(cornerRadius: 25.0)
-                .fill(.secondary)
+                .fill(.thinMaterial)
                 HStack {
                     AsyncImage(url: recipe.strMealThumb){result in result.image?
                             .resizable()
@@ -21,25 +23,23 @@ struct Card: View {
                     }.frame(minWidth: 50, idealWidth: 100, maxWidth: 150, minHeight: 50, idealHeight: 100, maxHeight: 150)
                         .clipShape(RoundedRectangle(cornerRadius: 25.0))
                     VStack(alignment: .leading) {
-                        Text("Recipe Name")
+                        Text(recipe.strMeal)
                             .font(.subheadline)
-                        Text("Category")
+                        Text(recipe.strCategory)
                             .font(.caption)
                         HStack {
                             Rectangle()
                                 .frame(minWidth: 30, idealWidth: 50, maxWidth: 50, minHeight: 10, idealHeight: 20, maxHeight: 30)
-                            Text("Country")
+                            Text(recipe.strArea)
                                 .font(.caption)
                         }
                         
                     }
                 }
         }
-        .padding(.all)
+        .padding(.horizontal)
             .frame(minHeight: 50, idealHeight: 100, maxHeight: 150)
     }
 }
 
-#Preview {
-    Card(recipe: Recipe(idMeal: "arrebiata", strMeal: "arrebiata", strCategory: "arrebiata", strArea: "arrebiata", strInstructions: "arrebiata", strMealThumb: URL(string: "https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg")!))
-}
+
--- GP4-Challenge-2/FeedView.swift
+++ GP4-Challenge-2/FeedView.swift
@@ -6,38 +6,15 @@ struct FeedView: View {
     
     var body: some View {
         NavigationView {
-            List {
+            ScrollView {
                 ForEach(recipeModel.recipesArray, id: \.idMeal) { meal in
-                    VStack {
-                        Card(recipe: meal)
-                        
-                        
-//                        HStack{
-//                            AsyncImage(url: meal.strMealThumb){result in result.image?
-//                                    .resizable()
-//                                    .scaledToFill()
-//                            }.frame(width: 100, height: 100)
-//                            
-//                            VStack(alignment: .leading){
-//                                Text(meal.strMeal)
-//                                    .font(.subheadline)
-//                                Text(meal.strCategory)
-//                                    .font(.caption)
-//                                    .foregroundStyle(Color(.gray))
-//                                Text(meal.strArea)
-//                                    .font(.caption)
-//                                    .foregroundStyle(Color(.gray))
-//                            }
-//                            
-//                        }
-                        }
-                        
-                }
-            }
+                    Card(recipe: meal)
+                }.listRowBackground(Color.secondary)
             .navigationTitle("Recipes")
             .searchable(text: $searchTerm, prompt: "Search recipe")
             .onChange(of: searchTerm) { _ in
                 recipeModel.fetchBySearch(nameRecipe: searchTerm)
+            }
             }
         }
     }
